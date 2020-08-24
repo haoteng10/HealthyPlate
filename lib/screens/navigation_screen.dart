443fcd81709d6ab44c82c4b7eb2ac483c9cb965c
Hao Teng <haoteng10@gmail.com>
2020-08-24 18:32:54 -0400

    Bottom Navigation Bar Updated & Fixed with Login/Logout Status

--- lib/screens/navigation_screen.dart
+++ lib/screens/navigation_screen.dart
@@ -1,6 +1,8 @@
 import "package:flutter/material.dart";
 import "package:nutrition/screens/login_screen.dart";
-import 'package:nutrition/screens/register_screen.dart';
+import "package:nutrition/screens/register_screen.dart";
+import "package:nutrition/screens/statistics_screen.dart";
+import "package:nutrition/screens/user_profile_screen.dart";
 import "package:provider/provider.dart";
 import "package:nutrition/models/user.dart";
 
@@ -13,7 +15,7 @@ class NavigationScreen extends StatefulWidget {
 
 class _NavigationScreenState extends State<NavigationScreen> {
   // Add global state to determine if the login button should change to account
-  bool _isLoggedIn;
+  bool _isLoggedIn = false;
 
   int _currentIndex = 0;
   @override
@@ -23,43 +25,80 @@ class _NavigationScreenState extends State<NavigationScreen> {
       //Got updated user data & user in not logged in view --> Change isLoggedIn to true
       setState(() {
         _isLoggedIn = true;
+        // After the user logged in, the user should be moved to the home screen
+        _currentIndex = 0;
       });
     } else if (user == null && _isLoggedIn == true) {
       //Got updated user data that is null & user has a logged in view --> Change to not logged in view
       setState(() {
         _isLoggedIn = false;
+        // After the user logged out, the user should be moved to the home screen
+        _currentIndex = 0;
       });
     }
 
-    return Scaffold(
-      bottomNavigationBar: BottomNavigationBar(
-        type: BottomNavigationBarType.fixed,
-        items: <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            title: Text("Home"),
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.add_circle_outline),
-            title: Text("Register"),
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.account_circle),
-            title: Text("Login"),
-          ),
-        ],
-        currentIndex: _currentIndex,
-        selectedItemColor: Colors.teal,
-        onTap: (int index) {
-          setState(() {
-            _currentIndex = index;
-          });
-        },
-      ),
-      body: IndexedStack(
-        index: _currentIndex,
-        children: [HomeScreen(), RegisterScreen(), LoginScreen()],
-      ),
-    );
+    if (!_isLoggedIn) {
+      return Scaffold(
+        bottomNavigationBar: BottomNavigationBar(
+          type: BottomNavigationBarType.fixed,
+          items: <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              title: Text("Home"),
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.add_circle_outline),
+              title: Text("Register"),
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.account_circle),
+              title: Text("Login"),
+            ),
+          ],
+          currentIndex: _currentIndex,
+          selectedItemColor: Colors.teal,
+          onTap: (int index) {
+            setState(() {
+              _currentIndex = index;
+            });
+          },
+        ),
+        body: IndexedStack(
+          index: _currentIndex,
+          children: [HomeScreen(), RegisterScreen(), LoginScreen()],
+        ),
+      );
+    } else {
+      return Scaffold(
+        bottomNavigationBar: BottomNavigationBar(
+          type: BottomNavigationBarType.fixed,
+          items: <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              title: Text("Home"),
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.table_chart),
+              title: Text("Statistics"),
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.person),
+              title: Text("Profile"),
+            ),
+          ],
+          currentIndex: _currentIndex,
+          selectedItemColor: Colors.teal,
+          onTap: (int index) {
+            setState(() {
+              _currentIndex = index;
+            });
+          },
+        ),
+        body: IndexedStack(
+          index: _currentIndex,
+          children: [HomeScreen(), StatisticsScreen(), UserProfileScreen()],
+        ),
+      );
+    }
   }
 }
