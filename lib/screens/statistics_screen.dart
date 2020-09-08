b0a6c496517332badaed205f4674a6608868bcd4
Hao Teng <haoteng10@gmail.com>
2020-09-08 13:53:33 -0400

    FL Chart Setup & Sample

--- lib/screens/statistics_screen.dart
+++ lib/screens/statistics_screen.dart
@@ -1,4 +1,5 @@
 import "package:flutter/material.dart";
+import 'package:nutrition/temp/bar_chart_sample_3.dart';
 
 class StatisticsScreen extends StatefulWidget {
   @override
@@ -8,6 +9,28 @@ class StatisticsScreen extends StatefulWidget {
 class _StatisticsScreenState extends State<StatisticsScreen> {
   @override
   Widget build(BuildContext context) {
-    return SafeArea(child: Text("Graphs & Charts should be here!"));
+    return SafeArea(
+        child: Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 24),
+      child: Column(
+        children: <Widget>[
+          RichText(
+            text: TextSpan(
+              style: Theme.of(context).textTheme.headline4,
+              children: <InlineSpan>[
+                TextSpan(
+                  text: "Calories",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                  ),
+                )
+              ],
+            ),
+          ),
+          SizedBox(height: 10),
+          BarChartSample3(),
+        ],
+      ),
+    ));
   }
 }
